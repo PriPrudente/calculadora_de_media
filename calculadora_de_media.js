66cbc70069e331f116a4d2565b504ba5f5229ed3
Priscila <priscila@ubuntu.local>
2021-09-13 19:36:44 -0300

    first commit

---
+++ calculadora_de_media.js
@@ -0,0 +1,18 @@
+var nome = "Priscila"
+
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 8
+var notaDoTerceiroBimestre = 9
+var notaDoQuartoBimestre = 7
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vinda " + nome)
+console.log(notaFinal)
+
+// isso é um comentário
+
+// Revisão :
+//variáveis, strings, console.log, toFixed , operações matemáticas , concatenação .
